6728e03bbcb3f5105f935332d3d1601da68da358
Stefano1991 <pieronistefano@hotmail.com>
2020-02-21 15:56:15 -0300

    Hero Jump and Input Handler

--- BemU/Assets/Scripts/Hero.cs
+++ BemU/Assets/Scripts/Hero.cs
@@ -11,10 +11,31 @@ public class Hero : MonoBehaviour
 
     public float speed = 2;
     public float walkSpeed = 2;
+    public float runSpeed = 5f;
+
+    bool isRunning;
+    bool isMoving;
+    float lastWalk;
+    public bool canRun = true;
+    float tapAgainToRunTime = 0.2f;
+    Vector3 lastWalkVector;
+
 
     Vector3 currentDir;
     bool isFacingLeft;
     protected Vector3 frontVector;
+
+
+    bool isJumpLandAnim;
+    bool isJumpingAnim;
+
+    public InputHandler input;
+
+    public float jumpForce = 1750f;
+    public float jumpDuration = 0.2f;
+    public float lastJumpTime;
+
+    public bool isGrounded;
 
     // Start is called before the first frame update
     void Start()
@@ -25,25 +46,60 @@ public class Hero : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float h = Input.GetAxisRaw("Horizontal");
-        float v = Input.GetAxisRaw("Vertical");
+
+        isJumpLandAnim = baseAnim.GetCurrentAnimatorStateInfo(0).IsName("jump_land");
+        isJumpingAnim = baseAnim.GetCurrentAnimatorStateInfo(0).IsName("jump_rise") || baseAnim.GetCurrentAnimatorStateInfo(0).IsName("jump_fall");
+
+        float h = input.GetHorizontalAxis();
+        float v = input.GetVerticalAxis();
+
+        bool jump = input.GetJumpButtonDown();
 
 
         currentDir = new Vector3(h, 0, v);
+        currentDir.Normalize();
 
         if (v == 0 && h == 0)
         {
             Stop();
-        } else if (v != 0 || h != 0)
+            isMoving = false;
+        } else if (!isMoving && (v != 0 || h != 0))
         {
-            Walk();
+            isMoving = true;
+            float dotProduct = Vector3.Dot(currentDir, lastWalkVector);
+            
+            if(canRun && Time.time < lastWalk + tapAgainToRunTime && dotProduct > 0)
+            {
+                Run();
+            } else
+            {
+                Walk();
+
+                if(h != 0)
+                {
+                    lastWalkVector = currentDir;
+                    lastWalk = Time.time;
+                }
+            }
         }
+
+        if (jump && !isJumpLandAnim && (isGrounded || (isJumpingAnim && Time.time < lastJumpTime +jumpDuration)))
+        {
+            Jump(currentDir);
+        }
+
+
+        Vector3 shadowSpritePosition = shadowSprite.transform.position;
+        shadowSpritePosition.y = 0;
+        shadowSprite.transform.position = shadowSpritePosition;
     }
 
     public void Stop()
     {
         speed = 0;
         baseAnim.SetFloat("Speed", speed);
+        isRunning = false;
+        baseAnim.SetBool("isRunning", isRunning);
     }
 
     public void Walk()
@@ -52,12 +108,59 @@ public class Hero : MonoBehaviour
         baseAnim.SetFloat("Speed", speed);
     }
 
+    public void Run()
+    {
+        speed = runSpeed;
+        isRunning = true;
+        baseAnim.SetBool("isRunning", isRunning);
+        baseAnim.SetFloat("Speed", speed);
+    }
+
+    void Jump(Vector3 direction)
+    {
+        if (!isJumpingAnim) {     baseAnim.SetTrigger ("Jump");
+            lastJumpTime = Time.time;
+ 
+            Vector3 horizontalVector = new Vector3(direction.x, 0, direction.z) * speed * 40;
+            body.AddForce(horizontalVector,ForceMode.Force);
+        }
+        Vector3 verticalVector = Vector3.up * jumpForce * Time.deltaTime;
+        body.AddForce(verticalVector,ForceMode.Force); 
+    }
+
+
+    void OnCollisionEnter(Collision collision) {
+        if (collision.collider.name == "Floor")
+        {
+            isGrounded = true;
+            baseAnim.SetBool("isGrounded", isGrounded);
+            DidLand();
+        }
+    }
+
+    void OnCollisionExit(Collision collision)
+    {
+        if (collision.collider.name == "Floor")
+        {
+            isGrounded = false;
+            baseAnim.SetBool("isGrounded", isGrounded);
+        }
+    } 
+
+    void DidLand()
+    {
+        Walk();
+    }
 
     private void FixedUpdate()
     {
         Vector3 moveVector = currentDir * speed;
-        body.MovePosition(transform.position + moveVector * Time.fixedDeltaTime);
-        baseAnim.SetFloat("Speed", moveVector.magnitude);
+
+        if (isGrounded)
+        {
+            body.MovePosition(transform.position + moveVector * Time.fixedDeltaTime);
+            baseAnim.SetFloat("Speed", moveVector.magnitude);
+        }
 
         if (moveVector != Vector3.zero)
         {
---
+++ BemU/Assets/Scripts/InputHandler.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputHandler : MonoBehaviour
+{
+
+    float horizontal;
+    float vertical;
+    bool jump;
+
+    float lastJumpTime;
+    bool isJumping;
+    public float maxJumpDuration = 0.2f;
+
+
+    public float GetVerticalAxis()
+    {
+        return vertical;
+    }
+    public float GetHorizontalAxis()
+    {
+        return horizontal;
+    }
+    public bool GetJumpButtonDown()
+    {
+        return jump;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+
+        if(!jump && !isJumping && Input.GetButton("Jump"))
+        {
+            jump = true;
+            lastJumpTime = Time.time;
+            isJumping = true;
+        } else if (!Input.GetButton("Jump"))
+        {
+            jump = false;
+            isJumping = false;
+        }
+
+        if(jump && Time.time > lastJumpTime + maxJumpDuration)
+        {
+            jump = false;
+        }
+    }
+}
